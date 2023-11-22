4732a01a37215d4222fc948d338fa6c63b1b75dc
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-11-22 20:18:47 +0200

    evelate individual profile to api

--- awsmedium/config.go
+++ awsmedium/config.go
@@ -17,36 +17,33 @@ import (
 //
 
 var (
-	Profiles = map[string][]medium.Profile{
-
+	ProfilePhoto = medium.Profiles(
 		//
-		// Digital Photography
-		//
-
-		"photo": medium.Profiles(
-			//
-			// Avatar
-			medium.On("av").Process(
-				medium.ScaleTo("small", 128, 128),
-				medium.ScaleTo("avatar", 400, 400),
-				medium.Replica("origin"),
-			),
-			//
-			// Wallpaper
-			medium.On("wp").Process(
-				medium.ScaleTo("equal", 1080, 1080),
-				medium.Replica("origin"),
-			),
-			//
-			// Digital Photo
-			medium.On("dp").Process(
-				medium.ScaleTo("small", 128, 128),
-				medium.ScaleTo("thumb", 240, 240),
-				medium.ScaleTo("cover", 480, 720),
-				medium.ScaleTo("equal", 1080, 1080),
-				medium.ScaleTo("large", 1080, 1920),
-				medium.Replica("origin"),
-			),
+		// Avatar
+		medium.On("av").Process(
+			medium.ScaleTo("small", 128, 128),
+			medium.ScaleTo("avatar", 400, 400),
+			medium.Replica("origin"),
 		),
+		//
+		// Wallpaper
+		medium.On("wp").Process(
+			medium.ScaleTo("equal", 1080, 1080),
+			medium.Replica("origin"),
+		),
+		//
+		// Digital Photo
+		medium.On("dp").Process(
+			medium.ScaleTo("small", 128, 128),
+			medium.ScaleTo("thumb", 240, 240),
+			medium.ScaleTo("cover", 480, 720),
+			medium.ScaleTo("equal", 1080, 1080),
+			medium.ScaleTo("large", 1080, 1920),
+			medium.Replica("origin"),
+		),
+	)
+
+	Profiles = map[string][]medium.Profile{
+		"photo": ProfilePhoto,
 	}
 )
